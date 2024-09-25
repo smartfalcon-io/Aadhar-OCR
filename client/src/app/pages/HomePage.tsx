d5304c409d450956d8529878daa7d47132817133
suhailabdaz <suhail.abdaz@gmail.com>
2024-09-26 01:02:52 +0530

    feat: setup_v_1.0.0

---
+++ client/src/app/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+ 
+ function HomePage() {
+   return (
+     <div className="font-extralight text-red-600 flex justify-center items-center">HomePage</div>
+   )
+ }
+ 
+ export default HomePage
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8001, // Replace with your desired port number
+  },
 })
