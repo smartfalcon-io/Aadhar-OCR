b499738ab79e96a1e8d72555b8cb24abc0706a1f
suhailabdaz <suhail.abdaz@gmail.com>
2024-09-28 23:04:04 +0530

    fix:provider

--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import Providers from './Provider.tsx'
+import Providers from './provider'
 import './index.css'
 
 const rootElement = document.getElementById('root')
